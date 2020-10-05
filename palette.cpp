3d904295530a84836036ecf3ea37746c10f8b3f4
Hinko Kocevar <hinko.kocevar@ess.eu>
2020-10-05 15:20:36 +0200

    add more palettes

--- palette.cpp
+++ palette.cpp
@@ -5,13 +5,8 @@
 #include <stdlib.h>
 #include <assert.h>
 
-Palette::Palette(const char *_name) {
-    name = _name;
-}
-
-Palette::~Palette() {
-
-}
+// palette definitions are taken from
+// https://github.com/kbinani/colormap-shaders
 
 float Palette::clamp(const float _v, const float _min, const float _max) {
     if (_v < _min) {
@@ -23,8 +18,36 @@ float Palette::clamp(const float _v, const float _min, const float _max) {
     }
 }
 
-Jet::Jet() : Palette("Jet") {}
-Jet::~Jet() {}
+void BlackWhite::setColor(const float _val, const float _scale, unsigned char *_map) {
+    float v = clamp(_val, 0.0, 1.0);
+
+    *(_map + 0)= (unsigned char)(v * _scale);
+    *(_map + 1)= (unsigned char)(v * _scale);
+    *(_map + 2)= (unsigned char)(v * _scale);
+    *(_map + 3)= (unsigned char)_scale;
+}
+
+void Cool::setColor(const float _val, const float _scale, unsigned char *_map) {
+    float r = (1.0 + 1.0 / 63.0) * _val - 1.0 / 63.0;
+    float g = -(1.0 + 1.0 / 63.0) * _val + (1.0 + 1.0 / 63.0);
+
+    *(_map + 0)= (unsigned char)(clamp(r, 0.0, 1.0) * _scale);
+    *(_map + 1)= (unsigned char)(clamp(g, 0.0, 1.0) * _scale);
+    *(_map + 2)= (unsigned char)(1.0 * _scale);
+    *(_map + 3)= (unsigned char)_scale;
+}
+
+void Hot::setColor(const float _val, const float _scale, unsigned char *_map) {
+    float r = (8.0 / 3.0 * _val);
+    float g = (8.0 / 3.0 * _val - 1.0);
+    float b = (4.0 * _val - 3.0);
+
+    *(_map + 0)= (unsigned char)(clamp(r, 0.0, 1.0) * _scale);
+    *(_map + 1)= (unsigned char)(clamp(g, 0.0, 1.0) * _scale);
+    *(_map + 2)= (unsigned char)(clamp(b, 0.0, 1.0) * _scale);
+    *(_map + 3)= (unsigned char)_scale;
+}
+
 void Jet::setColor(const float _val, const float _scale, unsigned char *_map) {
     float r;
     if (_val < 0.7) {
@@ -53,13 +76,120 @@ void Jet::setColor(const float _val, const float _scale, unsigned char *_map) {
     *(_map + 3)= (unsigned char)_scale;
 }
 
+void PurpleBlue::setColor(const float _val, const float _scale, unsigned char *_map) {
+    float r;
+    if (_val < 0.7520372909206926) {
+        r = (((9.68615208861418E+02 * _val - 1.16097242960380E+03) * _val + 1.06173672031378E+02) * _val - 1.68616613530379E+02) * _val + 2.56073136099945E+02;
+    } else {
+        r = -1.20830453148990E+01 * _val + 1.44337397593436E+01;
+    }
+
+    float g;
+    if (_val < 0.7485333535031721) {
+        g = (((-4.58537247030064E+02 * _val + 5.67323181593790E+02) * _val - 2.56714665792882E+02) * _val - 1.14205365680507E+02) * _val + 2.47073841488433E+02;
+    } else {
+        g = ((-2.99774273328017E+02 * _val + 4.12147041403012E+02) * _val - 2.49880079288168E+02) * _val + 1.93578601034431E+02;
+    }
+
+    float b;
+    if (_val < 0.7628468501376879) {
+        b = ((-5.44257972228224E+01 * _val + 2.70890554876532E+01) * _val - 9.12766750739247E+01) * _val + 2.52166182860177E+02;
+    } else {
+        b = (((4.55621137729287E+04 * _val - 1.59960900638524E+05) * _val + 2.09530452721547E+05) * _val - 1.21704642900945E+05) * _val + 2.66644674068694E+04;
+    }
+
+    *(_map + 0)= (unsigned char)(clamp(r / 255.0, 0.0, 1.0) * _scale);
+    *(_map + 1)= (unsigned char)(clamp(g / 255.0, 0.0, 1.0) * _scale);
+    *(_map + 2)= (unsigned char)(clamp(b / 255.0, 0.0, 1.0) * _scale);
+    *(_map + 3)= (unsigned char)_scale;
+}
+
+void HotMetal::setColor(const float _val, const float _scale, unsigned char *_map) {
+    float r;
+    if (_val < 0.0) {
+        r = 0.0;
+    } else if (_val <= 0.57147) {
+        r = 446.22 * _val / 255.0;
+    } else {
+       r = 1.0;
+    }
+
+    float g;
+    if (_val < 0.6) {
+        g = 0.0;
+    } else if (_val <= 0.95) {
+        g = ((_val - 0.6) * 728.57) / 255.0;
+    } else {
+        g = 1.0;
+    }
+
+    float b = 0.0;
+
+    *(_map + 0)= (unsigned char)(r * _scale);
+    *(_map + 1)= (unsigned char)(g * _scale);
+    *(_map + 2)= (unsigned char)(b * _scale);
+    *(_map + 3)= (unsigned char)_scale;
+}
+
+void Autumn::setColor(const float _val, const float _scale, unsigned char *_map) {
+    float r = 1.0;
+    float g = _val;
+    float b = 0.0;
+
+    *(_map + 0)= (unsigned char)(r * _scale);
+    *(_map + 1)= (unsigned char)(clamp(g, 0.0, 1.0) * _scale);
+    *(_map + 2)= (unsigned char)(b * _scale);
+    *(_map + 3)= (unsigned char)_scale;
+}
+
+void Spring::setColor(const float _val, const float _scale, unsigned char *_map) {
+    float r = 1.0;
+    float g = _val;
+    float b = 1.0 - _val;
+
+    *(_map + 0)= (unsigned char)(r * _scale);
+    *(_map + 1)= (unsigned char)(clamp(g, 0.0, 1.0) * _scale);
+    *(_map + 2)= (unsigned char)(clamp(b, 0.0, 1.0) * _scale);
+    *(_map + 3)= (unsigned char)_scale;
+}
+
+void Summer::setColor(const float _val, const float _scale, unsigned char *_map) {
+    float r = _val;
+    float g = 0.5 * _val + 0.5;
+    float b = 0.4;
+
+    *(_map + 0)= (unsigned char)(clamp(r, 0.0, 1.0) * _scale);
+    *(_map + 1)= (unsigned char)(clamp(g, 0.0, 1.0) * _scale);
+    *(_map + 2)= (unsigned char)(b * _scale);
+    *(_map + 3)= (unsigned char)_scale;
+}
+
+void Winter::setColor(const float _val, const float _scale, unsigned char *_map) {
+    float r = 0.0;
+    float g = _val;
+    float b = -0.5 * _val + 1.0;
+
+    *(_map + 0)= (unsigned char)(r * _scale);
+    *(_map + 1)= (unsigned char)(clamp(g, 0.0, 1.0) * _scale);
+    *(_map + 2)= (unsigned char)(clamp(b, 0.0, 1.0) * _scale);
+    *(_map + 3)= (unsigned char)_scale;
+}
 
 PaletteList::PaletteList() {
     items.clear();
     map = NULL;
     mapSize = 0;
 
+    items.push_back(new BlackWhite());
+    items.push_back(new Cool());
+    items.push_back(new Hot());
     items.push_back(new Jet());
+    items.push_back(new PurpleBlue());
+    items.push_back(new HotMetal());
+    items.push_back(new Autumn());
+    items.push_back(new Spring());
+    items.push_back(new Summer());
+    items.push_back(new Winter());
 }
 
 PaletteList::~PaletteList() {
--- palette.h
+++ palette.h
@@ -6,17 +6,75 @@
 struct Palette {
     const char *name;
 
-    Palette(const char *_name);
-    virtual ~Palette();
+    Palette(const char *_name) {
+        name = _name;
+    }
+    virtual ~Palette() {}
     virtual void setColor(const float _val, const float _scale, unsigned char *_map) = 0;
     float clamp(const float _v, const float _min, const float _max);
 };
 
-struct Jet : Palette {
-    Jet();
-    ~Jet();
+struct BlackWhite: Palette {
+    BlackWhite() : Palette("Black White") {}
+    ~BlackWhite() {}
     void setColor(const float _val, const float _scale, unsigned char *_map);
 };
+
+struct Cool : Palette {
+    Cool() : Palette("Cool") {}
+    ~Cool() {}
+    void setColor(const float _val, const float _scale, unsigned char *_map);
+};
+
+struct Hot : Palette {
+    Hot() : Palette("Hot") {}
+    ~Hot() {}
+    void setColor(const float _val, const float _scale, unsigned char *_map);
+};
+
+struct Jet : Palette {
+    Jet() : Palette("Jet") {}
+    ~Jet() {}
+    void setColor(const float _val, const float _scale, unsigned char *_map);
+};
+
+struct PurpleBlue : Palette {
+    PurpleBlue() : Palette("Purple Blue") {}
+    ~PurpleBlue() {}
+    void setColor(const float _val, const float _scale, unsigned char *_map);
+};
+
+struct HotMetal : Palette {
+    HotMetal() : Palette("Hot Metal") {}
+    ~HotMetal() {}
+    void setColor(const float _val, const float _scale, unsigned char *_map);
+};
+
+struct Autumn : Palette {
+    Autumn() : Palette("Autumn") {}
+    ~Autumn() {}
+    void setColor(const float _val, const float _scale, unsigned char *_map);
+};
+
+struct Spring : Palette {
+    Spring() : Palette("Spring") {}
+    ~Spring() {}
+    void setColor(const float _val, const float _scale, unsigned char *_map);
+};
+
+struct Summer : Palette {
+    Summer() : Palette("Summer") {}
+    ~Summer() {}
+    void setColor(const float _val, const float _scale, unsigned char *_map);
+};
+
+struct Winter : Palette {
+    Winter() : Palette("Winter") {}
+    ~Winter() {}
+    void setColor(const float _val, const float _scale, unsigned char *_map);
+};
+
+
 
 struct PaletteList {
     std::vector<Palette *> items;
